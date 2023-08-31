307d28b54a6a20147116db1d03d9adbc4d6cf568
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-08-31 16:28:54 +0300

    fix: \

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from 'firebase/analytics'
+import { getAnalytics, isSupported } from 'firebase/analytics'
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
@@ -15,6 +15,6 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
+export const analytics = isSupported().then((yes) => (yes ? getAnalytics(app) : null))
 export const auth = getAuth(app)
 export const db = getFirestore(app)
